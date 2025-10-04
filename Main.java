0cb612ba6eed5dcb4fe7fa37f95fdb8ef47db9d6
akhilesh <akhileshkumar4953@gmail.com>
2025-10-04 17:34:23 +0530

    fourth commit

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
-
-public class Main{
-    public static void main(String[] args){
-      System.out.println("This is java program!!!!!!");
-      System.out.println("My New Changes...");
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is a Java program!!!!!!");
+        System.out.println("My New Changes...");
+        
+        Student student = new Student();
+        student.sayHello();
     }
-
 }
